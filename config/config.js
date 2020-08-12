3c5947d767a0098f12b719e1317b0b505678ac9a
Atieh Hamidi <atieh@MacBook-Pro.local>
2020-08-12 10:51:21 +0200

    Move database url to .env file

---
+++ config/config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  development: {
+    url: process.env.DATABASE_DEV,
+    dialect: "postgres",
+    operatorsAliases: "0",
+  },
+  test: {
+    username: "root",
+    password: null,
+    database: "database_test",
+    host: "127.0.0.1",
+    dialect: "postgres",
+    operatorsAliases: "0",
+  },
+  production: {
+    username: "root",
+    password: null,
+    database: "database_test",
+    host: "127.0.0.1",
+    dialect: "postgres",
+    operatorsAliases: "0",
+  },
+};
