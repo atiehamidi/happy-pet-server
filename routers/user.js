a14318df0c85c32a11044dcfc87c10b93d486ed9
Atieh Hamidi <atieh@MacBook-Pro.local>
2020-08-13 14:29:29 +0200

    added some endpoint

--- routers/user.js
+++ routers/user.js
@@ -2,7 +2,7 @@ const { Router } = require("express");
 const { toJWT } = require("../auth/jwt");
 const authMiddleware = require("../auth/middleware");
 const User = require("../models/").user;
-const Client = require("../models/").pet;
+const Pet = require("../models/").pet;
 
 const Type = require("../models").type;
 const router = new Router();
@@ -25,7 +25,7 @@ router.get("/type", async (req, res, next) => {
 
 router.get("/clients", async (req, res, next) => {
   try {
-    const clients = await Client.findAll();
+    const clients = await Pet.findAll();
 
     if (!clients) {
       return res.status(400).send({
@@ -33,6 +33,75 @@ router.get("/clients", async (req, res, next) => {
       });
     }
     return res.status(200).send(clients);
+  } catch (error) {
+    console.log(error);
+    return res.status(400).send({ message: "Something went wrong, sorry" });
+  }
+});
+
+router.get("/dashboard", authMiddleware, async (req, res, next) => {
+  try {
+    const user = await User.findByPk(req.user.id, { include: [Pet] });
+    if (user === null) {
+      return res.status(404).send({ message: "This user does not exist" });
+    }
+    if (!user.id === req.user.id) {
+      return res
+        .status(403)
+        .send({ message: "You are not authorized to update this homepage" });
+    }
+
+    return res.status(200).send(user);
+  } catch (error) {
+    console.log(error);
+    return res.status(400).send({ message: "Something went wrong, sorry" });
+  }
+});
+router.post("/newpet", authMiddleware, async (req, res, next) => {
+  try {
+    const user = await User.findByPk(req.user.id);
+
+    if (user === null) {
+      return res.status(404).send({ message: "This user does not exist" });
+    }
+
+    if (!user.id === req.user.id) {
+      return res
+        .status(403)
+        .send({ message: "You are not authorized to update this user" });
+    }
+    const {
+      type,
+      name,
+      birthday,
+      sex,
+      imageOfPet,
+      breed,
+      descriptionOfPet,
+    } = req.body;
+    if (
+      !type ||
+      !name ||
+      !birthday ||
+      !sex ||
+      !imageOfPet ||
+      !breed ||
+      !descriptionOfPet
+    ) {
+      return res.status(400).send({ message: "Please provide all fileds" });
+    }
+
+    const newPet = await Pet.create({
+      type,
+      name,
+      birthday,
+      sex,
+      imageOfPet,
+      breed,
+      descriptionOfPet,
+      userId: req.user.id,
+    });
+    return res.status(201).send({ message: "your New pet added", newPet });
   } catch (error) {
     console.log(error);
     return res.status(400).send({ message: "Something went wrong, sorry" });
