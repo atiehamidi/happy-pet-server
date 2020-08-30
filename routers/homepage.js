db68921ded46b48be1dfcbfaf4f6eb6009325cec
Atieh Hamidi <hamidiatiye@gmail.com>
2020-08-30 14:39:09 +0200

    added admin router and transfered endpoint /admin

--- routers/homepage.js
+++ routers/homepage.js
@@ -40,3 +40,5 @@ router.get("/clients", async (req, res, next) => {
     return res.status(400).send({ message: "Something went wrong, sorry" });
   }
 });
+
+module.exports = router;
--- routers/user.js
+++ routers/user.js
@@ -136,41 +136,4 @@ router.post("/newtypeorder/:id", authMiddleware, async (req, res, next) => {
   }
 });
 
-router.get("/admin", authMiddleware, async (req, res, next) => {
-  try {
-    const user = await User.findByPk(req.user.id);
-    const orders = await Order.findAll();
-    if (!user || !user.admin) {
-      return res.status(400).send({ message: "this user doesn't Admin" });
-    } else {
-      return res.status(201).send({ message: "success", orders });
-    }
-  } catch (error) {
-    console.log(error);
-    return res.status(400).send({ message: "Something went wrong, sorry" });
-  }
-});
-
-router.patch("/admin/:id", authMiddleware, async (req, res, next) => {
-  try {
-    const user = await User.findByPk(req.user.id);
-    const orders = await Order.findAll();
-    const order = await Order.findByPk(req.params.id);
-    if (false) {
-      return res.status(400).send({ message: "this user doesn't Admin" });
-    } else {
-      order.done
-        ? await order.update({ done: "false" })
-        : await order.update({ done: "true" });
-
-      return res
-        .status(201)
-        .send({ message: "statuses updated successfully", orders });
-    }
-  } catch (error) {
-    console.log(error);
-    return res.status(400).send({ message: "Something went wrong, sorry" });
-  }
-});
-
 module.exports = router;
